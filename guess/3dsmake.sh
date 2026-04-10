b84165c0694cbbeed209a1a10210e36eba3cbb9b
1MD0NK3Y <jacopino.toma@gmail.com>
2026-04-10 10:55:22 +0200

    idk 3

--- guess/3dsmake.sh
+++ guess/3dsmake.sh
@@ -3,3 +3,5 @@ export $(grep -v '^#' info.env | xargs)
 docker run --rm -v "$(pwd)":/src -w /src devkitpro/devkitarm /bin/bash -c "make clean && make"
 
 mv src.3dsx $GAME_NAME.3dsx
+
+./3dsclear.sh
